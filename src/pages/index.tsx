5be4529f7809521ac92b53559da1c7353696e264
João Marcelo <jjmarcelo2@gmail.com>
2023-02-27 10:12:38 -0300

    Limpando o projeto para fazermos o CRUD

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,5 @@
-import { useState } from "react";
-import { Lights } from "../components/Lights";
-
 export default function Index() {
-    const [ lightsIsOn, setLightsIsOn ] = useState<boolean>(false);
-
     return (
-        <div
-            className="flex flex-col items-center justify-center w-screen h-screen"
-        >
-            <button
-                type="button"
-                aria-label="Botão que liga e desliga as luzes"
-                className="p-4 mb-4 bg-slate-300"
-                onClick={() => setLightsIsOn(!lightsIsOn)}
-            >
-                {lightsIsOn ? "Desligar luzes" : "Ligar luzes"}
-            </button>
-            <Lights isOn={lightsIsOn} />
-        </div>
+        <h1>Olá Mundo</h1>
     );
 }
